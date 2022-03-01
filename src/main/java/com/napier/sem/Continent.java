0e13e27edb2f81273a64fd041ba364e89960002e
Stefan Humpelstetter <40493762@live.napier.ac.uk>
2022-03-01 12:24:20 +0100

    Add Country and Continent model

---
+++ src/main/java/com/napier/sem/Continent.java
@@ -0,0 +1,11 @@
+package com.napier.sem;
+
+public enum Continent {
+    Asia,
+    Europe,
+    North_America,
+    Africa,
+    Oceania,
+    Antarctica,
+    South_America
+}
---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,35 @@
+package com.napier.sem;
+
+public class Country {
+
+    public String code;
+
+    public String name;
+
+    public Continent continent;
+
+    public String region;
+
+    public int surfaceArea;
+
+    public int indepYear;
+
+    public int population;
+
+    public int lifeExpectancy;
+
+    public float gnp;
+
+    public float gnpOld;
+
+    public String localName;
+
+    public String governmentForm;
+
+    public String headOfState;
+
+    public String capital;
+
+    public String code2;
+
+}
