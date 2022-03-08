2ef4f7c3b1fdd07d5c9f64da68791b5a3730beae
Stefan Humpelstetter <40493762@live.napier.ac.uk>
2022-03-08 12:37:48 +0100

    Add comments to AModel

--- src/main/java/com/napier/sem/Models/AModel.java
+++ src/main/java/com/napier/sem/Models/AModel.java
@@ -3,14 +3,29 @@ package com.napier.sem.Models;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Abstract representation of a model
+ */
 public abstract class AModel {
 
+    /**
+     * Model's ResultSet that it will be constructed from
+     */
     protected ResultSet rset;
 
+    /**
+     * constructor takes a ResultSet to create the model
+     * @param rset representation of the model in the database
+     * @throws SQLException
+     */
     public AModel(ResultSet rset) throws SQLException {
         this.rset=rset;
         construct();
     }
 
+    /**
+     * Method to construct the model
+     * @throws SQLException
+     */
     protected abstract void construct() throws SQLException;
 }
