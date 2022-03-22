6d44bee7317d49fcf9c24b8e0d8ac4a248a5b115
Stefan Humpelstetter <40493762@live.napier.ac.uk>
2022-03-22 11:27:30 +0100

    Update github actions

--- src/test/java/com/napier/sem/CapitalCityTest.java
+++ src/test/java/com/napier/sem/CapitalCityTest.java
@@ -1,11 +1,16 @@
 package com.napier.sem;
 
+import com.napier.sem.Models.City;
 import com.napier.sem.Models.Continent;
 import com.napier.sem.Reports.CapitalCityReport;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class CapitalCityTest {
@@ -23,8 +28,10 @@ public class CapitalCityTest {
     @Test
     void testGetTopNPopulatedCapitalCitiesInAContinent()
     {
-        capitalCityReport.getTopNPopulatedCapitalCitiesInAContinent(Continent.Africa,5);
+        ArrayList<City> cities = capitalCityReport.getTopNPopulatedCapitalCitiesInAContinent(Continent.Africa,5);
 
+        // test if return list has correct size
+        assertEquals(5,cities.size());
     }
 
     @AfterAll
